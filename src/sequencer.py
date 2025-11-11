73fa274bbf53e724a6532e1ad5eee4ce7bfc6a6f
jackkaiser1 <jackkaiser78@gmail.com>
2025-11-11 14:01:16 -0500

    Initialized widget class files / StepButton and SequencerFrame class

---
+++ src/sequencer.py
@@ -0,0 +1,13 @@
+import customtkinter 
+
+class StepButton(customtkinter.CTkbutton):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class SequencerFrame(customtkinter.CTkFrame):
+    def __init__(self, parent):
+        super().__init(parent)
+        pass
+
+
