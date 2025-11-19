7fb528fc81a28ff745f9a7f5ad483f4476b9e487
jackkaiser1 <jackkaiser78@gmail.com>
2025-11-19 16:11:24 -0500

    Created test suite for select_drum() and helper functions

--- test/test_sequencer.py
+++ test/test_sequencer.py
@@ -6,68 +6,150 @@ sys.path.append("/home/jackk/dev/github.com/jackkaiser1/py_808/src/assets")
 import unittest
 import customtkinter 
 from sequencer import SequencerFrame, StepButton
+from sound_select import SoundSelectFrame
 from constants import *
 from src.assets import *
 from sound_mixer import drum
 
-root = customtkinter.CTk()
-frame = SequencerFrame(root)
-frame.grid(row=0, column=0)
-frame.create()
+# root = customtkinter.CTk()
+# frame = SequencerFrame(root)
+# frame.grid(row=0, column=0)
+# frame.create()
+
+class TestRoot(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()     
+
+        # Init root window -------------------------------------------
+        self.title("PY-808")
+        self.geometry("1220x650")
+
+        # Init frames
+        self.sequencer = SequencerFrame(self)
+        self.sound_select_buttons = SoundSelectFrame(self)
+
+        # Position frames
+        self.sequencer.grid(row=3, column=0, pady=10)
+        self.sound_select_buttons.grid(row=2, column=0, pady=10)
+
+        # Center widgets in column 0
+        self.grid_columnconfigure(0, weight=1)
+
+root = TestRoot()
+root.withdraw()
+
+root.sequencer.create()
+root.sound_select_buttons.create(root)
 
 class TestSequencerFrame(unittest.TestCase):
     
     # Tests for create() ----------------------------------------
     def test_create_text(self):
-        self.assertEqual("4", frame.step_list[3]._text)
+        self.assertEqual("4", root.sequencer.step_list[3]._text)
 
     def test_create_color_grey1(self):
-        self.assertEqual(GREY_1, frame.step_list[1]._fg_color)
+        self.assertEqual(GREY_1, root.sequencer.step_list[1]._fg_color)
 
     def test_create_color_grey2(self):
-        self.assertEqual(GREY_2, frame.step_list[4]._fg_color)
+        self.assertEqual(GREY_2, root.sequencer.step_list[4]._fg_color)
 
 
     # Tests for position() ----------------------------------------
     def test_position_column(self):
-        grid_info = frame.step_list[5].grid_info()
+        grid_info = root.sequencer.step_list[5].grid_info()
         self.assertEqual(5, grid_info["column"])
 
     def test_position_row(self):
-        grid_info = frame.step_list[8].grid_info()
+        grid_info = root.sequencer.step_list[8].grid_info()
         self.assertEqual(0, grid_info["row"])
 
     def test_position_padx(self):
-        grid_info = frame.step_list[5].grid_info()
+        grid_info = root.sequencer.step_list[5].grid_info()
         self.assertEqual(STEP_PAD_X, grid_info["padx"])
 
     def test_position_pady(self):
-        grid_info = frame.step_list[9].grid_info()
+        grid_info = root.sequencer.step_list[9].grid_info()
         self.assertEqual(STEP_PAD_Y, grid_info["pady"])
+
+    # Test display_pattern
+    def test_display_pattern_snare(self):
+        snare_button = root.sound_select_buttons.sound_buttons[1]
+        kick_button = root.sound_select_buttons.sound_buttons[0]
+        root.sound_select_buttons.select_drum(snare_button, root)
+
+        beat_list = drum.current_drum.beat_list
+        beat_list.add(3)
+        beat_list.add(7)
+
+        root.sound_select_buttons.select_drum(kick_button, root)
+
+        beat_list = drum.current_drum.beat_list
+        beat_list.add(4)
+        beat_list.add(10)
+
+        root.sound_select_buttons.select_drum(snare_button, root)
+
+        self.assertEqual(ORANGE_1, root.sequencer.step_list[2]._fg_color)
+        self.assertEqual(ORANGE_2, root.sequencer.step_list[6]._fg_color)
+
+        beat_list.clear()
+        root.sound_select_buttons.select_drum(kick_button, root)
+        beat_list = drum.current_drum.beat_list
+        beat_list.clear()
+
+
+    def test_display_pattern_kick(self):
+        snare_button = root.sound_select_buttons.sound_buttons[1]
+        kick_button = root.sound_select_buttons.sound_buttons[0]
+        root.sound_select_buttons.select_drum(snare_button, root)
+
+        beat_list = drum.current_drum.beat_list
+        beat_list.add(3)
+        beat_list.add(7)
+
+        root.sound_select_buttons.select_drum(kick_button, root)
+
+        beat_list = drum.current_drum.beat_list
+        beat_list.add(4)
+        beat_list.add(10)
+
+        root.sound_select_buttons.select_drum(snare_button, root)
+        root.sound_select_buttons.select_drum(kick_button, root)
+
+        self.assertEqual(ORANGE_1, root.sequencer.step_list[3]._fg_color)
+        self.assertEqual(ORANGE_1, root.sequencer.step_list[9]._fg_color)
+
+        beat_list.clear()
+        root.sound_select_buttons.select_drum(kick_button, root)
+        beat_list = drum.current_drum.beat_list
+        beat_list.clear()
+
+
+
 
 
 class TestStepButton(unittest.TestCase):
     def test_populate_steps_orange_1(self):
-        step_2 = frame.step_list[1]
+        step_2 = root.sequencer.step_list[1]
         step_2.populate_step(step_2, drum.current_drum)
         self.assertEqual(ORANGE_1, step_2._fg_color)
         drum.current_drum.beat_list.clear()
 
     def test_populate_steps_orange_2(self):
-        step_6 = frame.step_list[5]
+        step_6 = root.sequencer.step_list[5]
         step_6.populate_step(step_6, drum.current_drum)
         self.assertEqual(ORANGE_2, step_6._fg_color)
         drum.current_drum.beat_list.clear()
 
     def test_populate_steps_grey_1(self):
-        step_2 = frame.step_list[1]
+        step_2 = root.sequencer.step_list[1]
         step_2.populate_step(step_2, drum.current_drum)
         step_2.populate_step(step_2, drum.current_drum)
         self.assertEqual(GREY_1, step_2._fg_color)
         drum.current_drum.beat_list.clear()
 
     def test_populate_steps_grey_2(self):
-        step_6 = frame.step_list[5]
+        step_6 = root.sequencer.step_list[5]
         step_6.populate_step(step_6, drum.current_drum)
         step_6.populate_step(step_6, drum.current_drum)
         self.assertEqual(GREY_2, step_6._fg_color)
--- test/test_sound_select.py
+++ test/test_sound_select.py
@@ -2,44 +2,128 @@ import sys
 sys.path.append("/home/jackk/dev/github.com/jackkaiser1/py_808/src")
 
 import unittest
+from unittest.mock import patch, MagicMock
 import customtkinter
 from sound_select import SoundSelectFrame
 from constants import *
+from sound_mixer import drum, snare, hihat, tom_hi
+from sequencer import SequencerFrame
 
-root = customtkinter.CTk()
-frame = SoundSelectFrame(root)
-frame.grid(row=0, column=0)
-frame.create(frame)
+
+class TestRoot(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()     
+
+        # Init root window -------------------------------------------
+        self.title("PY-808")
+        self.geometry("1220x650")
+
+        # Init frames
+        self.sequencer = SequencerFrame(self)
+        self.sound_select_buttons = SoundSelectFrame(self)
+
+        # Position frames
+        self.sequencer.grid(row=3, column=0, pady=10)
+        self.sound_select_buttons.grid(row=2, column=0, pady=10)
+
+        # Center widgets in column 0
+        self.grid_columnconfigure(0, weight=1)
+
+root = TestRoot()
+root.withdraw()
+
+root.sequencer.create()
+root.sound_select_buttons.create(root)
 
 class TestSoundSelectFrame(unittest.TestCase):
+
     # Tests for create() ----------------------------------------
     def test_create_drum_name_1(self):
-        button_4_drum_name = frame.sound_buttons[3]._text
+        button_4_drum_name = root.sound_select_buttons.sound_buttons[3]._text
         self.assertEqual("HHAT", button_4_drum_name)
 
     def test_create_drum_name_2(self):
-        button_8_drum_name = frame.sound_buttons[7]._text
+        button_8_drum_name = root.sound_select_buttons.sound_buttons[7]._text
         self.assertEqual("OHAT", button_8_drum_name)
 
     def test_create_drum_name_3(self):
-        button_11_drum_name = frame.sound_buttons[10]._text
+        button_11_drum_name = root.sound_select_buttons.sound_buttons[10]._text
         self.assertEqual("TOM L", button_11_drum_name)
 
 
 
     # Tests for position() ----------------------------------------
     def test_position_column(self):
-        sound_button_column = frame.sound_buttons[4].grid_info()["column"]
+        sound_button_column = root.sound_select_buttons.sound_buttons[4].grid_info()["column"]
         self.assertEqual(4, sound_button_column)
 
     def test_position_row(self):
-        sound_button_row = frame.sound_buttons[11].grid_info()["row"]
+        sound_button_row = root.sound_select_buttons.sound_buttons[11].grid_info()["row"]
         self.assertEqual(0, sound_button_row)
 
     def test_position_padx(self):
-        sound_button_padx = frame.sound_buttons[3].grid_info()["padx"]
+        sound_button_padx = root.sound_select_buttons.sound_buttons[3].grid_info()["padx"]
         self.assertEqual(SOUND_BUTTON_PAD_X, sound_button_padx)
 
     def test_position_pady(self):
-        sound_button_pady = frame.sound_buttons[9].grid_info()["pady"]
+        sound_button_pady = root.sound_select_buttons.sound_buttons[9].grid_info()["pady"]
         self.assertEqual(SOUND_BUTTON_PAD_Y, sound_button_pady)
+
+
+    # Test init_command and set_command
+    def test_set_command_1(self):
+        sound_button_command_1 = root.sound_select_buttons.sound_buttons[4]._command
+        self.assertIsNotNone(sound_button_command_1)
+
+    def test_set_command_2(self):
+        sound_button_command_2 = root.sound_select_buttons.sound_buttons[8]._command
+        self.assertIsNotNone(sound_button_command_2)
+
+    # Test drum_select
+    # ----- Test current drum
+    def test_select_drum_snare(self):
+        snare_button = root.sound_select_buttons.sound_buttons[1]
+        root.sound_select_buttons.select_drum(snare_button, root)
+
+        current_drum = drum.current_drum
+        
+        self.assertEqual(snare, current_drum)
+
+    def test_select_drum_hihat(self):
+        hihat_button = root.sound_select_buttons.sound_buttons[3]
+        root.sound_select_buttons.select_drum(hihat_button, root)
+
+        current_drum = drum.current_drum
+        
+        self.assertEqual(hihat, current_drum)
+
+    def test_select_drum_tom_hi(self):
+        tom_hi_button = root.sound_select_buttons.sound_buttons[12]
+        root.sound_select_buttons.select_drum(tom_hi_button, root)
+
+        current_drum = drum.current_drum
+        
+        self.assertEqual(tom_hi, current_drum)
+
+    # ----- Test activate_button
+    def test_activate_button_snare(self):
+        snare_button = root.sound_select_buttons.sound_buttons[1]
+        root.sound_select_buttons.select_drum(snare_button, root)
+
+        self.assertEqual(ORANGE_1, snare_button._fg_color)
+
+    def test_activate_button_hihat(self):
+        hihat_button = root.sound_select_buttons.sound_buttons[3]
+        root.sound_select_buttons.select_drum(hihat_button, root)
+
+        self.assertEqual(ORANGE_1, hihat_button._fg_color) 
+
+    def test_activate_button_tom_hi(self):
+        tom_hi_button = root.sound_select_buttons.sound_buttons[12]
+        root.sound_select_buttons.select_drum(tom_hi_button, root)
+
+        self.assertEqual(ORANGE_1, tom_hi_button._fg_color) 
+
+
+         
+    
