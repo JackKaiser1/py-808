6faf6f31dee11728608e0cca5a94b62ffe5ffd40
jackkaiser1 <jackkaiser78@gmail.com>
2025-11-26 15:40:12 -0500

    Wrote tests for set_bpm()

--- test/test_bpm_slider.py
+++ test/test_bpm_slider.py
@@ -5,12 +5,48 @@ import unittest
 import customtkinter 
 from constants import *
 from bpm_slider import BPMSliderFrame
+from sequencer import StepButton, SequencerFrame
+from sound_select import SoundSelectFrame, SoundSelectButton
+from faders import VolumeFaderFrame, VolumeFader
+from control_buttons_frame import ControlButtonsFrame
 
-root = customtkinter.CTk()
-frame = BPMSliderFrame(root)
-frame.bpm_slider.set(70)
-frame.grid(row=0, column=0)
+class TestRoot(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()     
+
+        # Init root window -------------------------------------------
+        self.title("PY-808")
+        self.geometry("1220x650")
+
+        # Init frames
+        self.sequencer = SequencerFrame(self)
+        self.sound_select_buttons = SoundSelectFrame(self)
+        self.faders = VolumeFaderFrame(self)
+        self.control_buttons = ControlButtonsFrame(self)
+
+        # Position frames
+        self.sequencer.grid(row=3, column=0, pady=10)
+        self.sound_select_buttons.grid(row=2, column=0, pady=10)
+        self.faders.grid(row=1, column=0, pady=(20, 10))
+        self.control_buttons.grid(row=0, column=0, pady=(60, 10))
+
+        # Center widgets in column 0
+        self.grid_columnconfigure(0, weight=1)
+
+root = TestRoot()
+root.sequencer.create()
+root.sound_select_buttons.create(root)
+root.faders.create()
+
 
 class TestBPMSliderFrame(unittest.TestCase):
     def test_bpm_slider_label(self):
-        self.assertEqual(70, frame.bpm_slider.get())
+        self.assertEqual(130, root.control_buttons.bpm.bpm_slider.get())
+
+    def test_set_BPM_1(self):
+        interval = root.control_buttons.bpm.set_bpm(70, root)
+        self.assertEqual(0.214, interval)
+        
+    def test_set_BPM_2(self):
+        interval = root.control_buttons.bpm.set_bpm(140, root)
+        self.assertEqual(0.107, interval)
